688c07f1b1419d5b04a32f1947b908c03566b57c
Jianqi Lin <DorianLin@outlook.com>
2022-04-01 13:02:14 -0400

    updated homeview

--- TripX/Map/MapHomeView.swift
+++ TripX/Map/MapHomeView.swift
@@ -18,6 +18,50 @@ struct MapHomeView: View {
             MapView() {
                 
             }
+            if viewmodel.routeEvents.count > 0 {
+                VStack {
+                    HStack {
+                        EmptyView()
+                        Spacer()
+                        Text(viewmodel.trip.name)
+                            .font(.title2)
+                        Spacer()
+                        Button(action: {
+                            presentationMode.wrappedValue.dismiss()
+                        }) {
+                            Image(systemName: "xmark.circle.fill")
+                        }
+                        .frame(width: 40, height: 40, alignment: .center)
+                    }
+                    .frame(maxWidth: .infinity)
+                    
+                    HStack {
+                        Button(action: {
+                            viewmodel.preparePreviousRoute()
+                        }) {
+                            Image(systemName: "chevron.left")
+                        }
+                        
+                        Text(viewmodel.routeDate)
+                            .padding(.horizontal)
+                            .padding(.vertical, 5)
+                            .background(
+                                RoundedRectangle(cornerRadius: 20)
+                                    .fill(Color.white)
+                            )
+                            .frame(height: 60)
+                        
+                        Button(action: {
+                            viewmodel.prepareNextRoute()
+                        }) {
+                            Image(systemName: "chevron.right")
+                        }
+                    }
+                    .padding(.top, -10)
+                }
+                .padding(.top, 20)
+                .zIndex(9999)
+            }
         }
     }
     
